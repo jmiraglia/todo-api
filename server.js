aaa0ad1eee4994b551bc168a195c210e055bb151
Jason Miraglia <jmiraglia@interstateaerials.com>
2016-06-14 15:48:01 -0400

    Add ?completed filter

--- server.js
+++ server.js
@@ -15,14 +15,27 @@ app.get('/', function(req, res){
 
 // return all todo items
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var query_params = req.query,
+        filtered_todos = todos,
+        filter;
+
+    if(query_params.hasOwnProperty('completed') && query_params.completed === 'true'){
+        filter = {completed: true};
+    } else if (query_params.hasOwnProperty('completed') && query_params.completed === 'true'){
+        filter = {completed: false};
+    }
+
+    if(filter){
+        filtered_todos = _.where(filtered_todos, filter);
+    }
+
+    res.json(filtered_todos);
 });
 
 // return a single todo item
 app.get('/todos/:id', function(req, res){
     var todo_id = parseInt(req.params.id, 10),
         matched_todo;
-
     // Find the requested todo by id
     matched_todo = _.findWhere(todos, {id: todo_id});
 
@@ -53,12 +66,41 @@ app.delete('/todos/:id', function(req, res){
         matched_todo = _.findWhere(todos, {id: todo_id});
 
     if(matched_todo){
-        // Remove the matched_todo from array
+        // Remove the matched_todo from array of todos
         todos = _.without(todos, matched_todo);
         res.status(200).json(matched_todo);
     } else {
         res.status(404).json({"error": "no todo found with that id"});
     }
+});
+
+app.put('/todos/:id', function(req, res){
+    var body = _.pick(req.body, 'description', 'completed'),
+       valid_attributes = {},
+       todo_id = parseInt(req.params.id, 10),
+       matched_todo = _.findWhere(todos, {id: todo_id});
+
+    if(!matched_todo){
+        return res.status(404).json({"error": "no todo found with that id"});
+    }
+
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        valid_attributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed') && !_.isBoolean(body.completed)){
+        return res.status(400).json({"error": "invalid data type for [completed]"});
+    }
+
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        valid_attributes.description = body.description.trim();
+    } else if(body.hasOwnProperty('description') && !_.isString(body.description)){
+        return res.status(400).json({"error": "invalid data type for [description]"});
+    }
+
+
+    // overwrite with new values
+    _.extend(matched_todo, valid_attributes);
+
+    return res.status(200).json(matched_todo);
 });
 
 app.listen(PORT, function(){
