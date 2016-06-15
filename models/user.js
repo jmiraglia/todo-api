7dabe2ec10956bd86d44b87bb2035a24f1a7f3e5
Jason Miraglia <jmiraglia@interstateaerials.com>
2016-06-15 14:22:56 -0400

    Add user model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+module.exports = function (sql, DataTypes) {
+    return sql.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8, 100]
+            }
+        }
+    });
+};
